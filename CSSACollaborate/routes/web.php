9eb8104c76a470b047075adc210016c406e84db3
Shehryyy <143545797+Shehryyy@users.noreply.github.com>
2025-08-12 19:51:37 -0400

    SignUp page

--- CSSACollaborate/routes/web.php
+++ CSSACollaborate/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/signUp', function () {
+    return view('signUp'); 
+})->name('signUp.form');
+
