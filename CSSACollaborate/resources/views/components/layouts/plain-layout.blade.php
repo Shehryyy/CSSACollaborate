987ca489d7e26f0df032cace672fcf521f926473
Danat Ali Muradov <danatmuradov500@gmail.com>
2025-08-13 15:10:32 -0400

    Plain layout: Adjusted layout.

--- CSSACollaborate/resources/views/components/layouts/plain-layout.blade.php
+++ CSSACollaborate/resources/views/components/layouts/plain-layout.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Inter'>
     <link rel="stylesheet" href="{{ asset('css/layouts/plain-layout.css') }}">
     <title>{{ $title }}</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
