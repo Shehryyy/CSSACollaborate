4094b8f64bd6996a682cea0af959ca721f32c2b0
Danat Ali Muradov <danatmuradov500@gmail.com>
2025-08-17 17:46:50 -0400

    Views: Started work on the sidebar component.

--- CSSACollaborate/resources/views/components/layouts/main-layout.blade.php
+++ CSSACollaborate/resources/views/components/layouts/main-layout.blade.php
@@ -1,5 +1,6 @@
 @props(['title' => 'CSSACollaborate'])
 
 <x-layouts.plain-layout :title="$title">
-    <x-panels.topbar />
+    <x-panels.topbar/>
+    <x-panels.sidebar/>
 </x-layouts.plain-layout>
---
+++ CSSACollaborate/resources/views/components/panels/sidebar.blade.php
@@ -0,0 +1,5 @@
+@props(['selectedTab' => 'collaborate'])
+
+<div id="sidebar">
+    <img src="{{ Vite::asset('resources/images/vaniercssa_logo.png') }}" alt="Vanier CSSA Logo" class="profile-picture">
+</div>
