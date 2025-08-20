8d00d01710b72b7727e9cd59e6da1cdef80b1c67
Danat Ali Muradov <danatmuradov500@gmail.com>
2025-08-20 14:34:48 -0400

    Collaborate: Created the empty collaborate page.

--- CSSACollaborate/resources/views/collaborate.blade.php
+++ CSSACollaborate/resources/views/collaborate.blade.php
@@ -1,3 +1,12 @@
-<x-layouts.main-layout title="Collaborate - CSSACollaborate">
+@props(['projects' => []])
 
+<x-layouts.main-layout title="Collaborate - CSSACollaborate">
+    @if(!empty($projects))
+        
+    @else 
+        <div class="empty-collaborate-container">
+            <h3>Projects seeking teammates appear here</h3>
+            <img src="{{ Vite::asset('resources/images/icons/collaborate_icon.png') }}">
+        </div> 
+    @endif
 </x-layouts.main-layout>
