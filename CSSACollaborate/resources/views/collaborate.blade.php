fd300bd1ba8d9e7835964d10b7589230994a5779
Danat Ali Muradov <danatmuradov500@gmail.com>
2025-08-20 21:39:21 -0400

    Collaborate page: continued work on collaborate page.

--- CSSACollaborate/resources/views/collaborate.blade.php
+++ CSSACollaborate/resources/views/collaborate.blade.php
@@ -1,8 +1,12 @@
-@props(['projects' => []])
+@props(['projects' => ['1']])
 
 <x-layouts.main-layout title="Collaborate - CSSACollaborate">
     @if(!empty($projects))
+        {{-- This will be populated based on the projects in the $projects variable --}}
+        {{-- For now, example projects will be added for the purpose of developing the front-end --}}
+        <div class="collaborate-projects-container">
         
+        </div>
     @else 
         <div class="empty-collaborate-container">
             <h3>Projects seeking teammates appear here</h3>
